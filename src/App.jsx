919055b5c99381a5145fa11469dd194e06696121
Akhil <rawatakhil5624@gmail.com>
2024-02-27 01:01:26 +0530

    updated image loaction

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ function App() {
   return (
     <>
       <div className="header">
-        <img src="src/assets/logo.png" alt="" className='banner' />
+        <img src="images/logo.png" alt="" className='banner' />
       </div>
       <div className="description">
         <div className="trophy">
-          <img src="src/assets/1.png" alt="" className='trophy' />
+          <img src="images/1.png" alt="" className='trophy' />
         </div>
         <div className="content">
           <h4> C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h4>
@@ -30,13 +30,13 @@ function App() {
             <li> C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
             <li>  C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. </li>
           </ul>
-          <img src="src/assets/2.png" alt="" className='pic2'/>
+          <img src="images/2.png" alt="" className='pic2'/>
           <p>Government of India has awarded the <b> "National Energy Conservation Award 2018" </b>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
         </div>
       </div>
       <p className='end'>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION. </p>
       <div className="supplies">
-        <img src="src/assets/3.png" alt="" className='supplies-img' />
+        <img src="images/3.png" alt="" className='supplies-img' />
         <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors </p>
       </div>
       <hr />
